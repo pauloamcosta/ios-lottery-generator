c4d9f470513534b2ee4297e38285f7e6fccdeac0
Paulo Andre Melo Costa <pauloamcosta2@gmail.comgit>
2018-10-09 02:06:27 -0300

    Initial Commit

---
+++ Lottery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lottery
+//
+//  Created by Paulo Costa on 09/10/2018.
+//  Copyright © 2018 Paulo Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
