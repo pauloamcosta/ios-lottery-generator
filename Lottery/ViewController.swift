f1bc1d011111fd52b853525be02789c2aafecae8
Paulo Andre Melo Costa <pauloamcosta2@gmail.comgit>
2018-10-10 12:08:44 -0300

    bulding lottery balls

--- Lottery/ViewController.swift
+++ Lottery/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    @IBOutlet var ball: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func generateGame() {
+    }
+    
 }
 
